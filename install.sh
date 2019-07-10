65700589c8e6bcb74edd7f162bb84a5c12c2b510
Joel Tio <joeltiojinhon@gmail.com>
2019-07-10 15:29:29 +0800

    Fix install.sh not adding vagrant user to docker group

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ sudo snap refresh
 
 # Create a docker group and add the current user to the group
 sudo groupadd docker
-sudo usermod -aG docker $USER
+sudo usermod -aG docker vagrant
 # Install Docker
 sudo snap install docker
